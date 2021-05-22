7499d17875a0911e632fed9f5ee3717d253f14e2
Alexey Mefodyev <al.mefodyev@yandex.ru>
2021-05-22 14:56:28 +0300

    Initial Commit

---
+++ ColorSwitcher/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ColorSwitcher
+//
+//  Created by Alexey Mefodyev on 22.05.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
